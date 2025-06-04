f1c0570cb9e81bff04d7acde9ab21c00fc589dbe
dzmitryNz <d.nayezzhy@gmail.com>
2025-06-04 09:21:54 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as YAML from 'yaml';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 4000);
+
+  const swaggerYaml = fs.readFileSync(
+    path.join(__dirname, '../doc/api.yaml'),
+    'utf8',
+  );
+  const swaggerDocument = YAML.parse(swaggerYaml);
+
+  SwaggerModule.setup('doc', app, swaggerDocument);
+
   await app.listen(port);
 }
 bootstrap();
